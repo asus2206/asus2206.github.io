27da1cb2b4310c1b5a46ee9cf08bf2106352b6ea
asus2206 <Lorenz@Lorenzs-MBP.lan>
2015-01-24 00:50:51 +0100

    Neue Page
    vieles geht nicht

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,84 +1,41 @@
+var project = new Array( "radiopreis", "ctv", "mapple", "python", "raspberry", "portfolio" );
+var skill = new Array( "code", "design", "usability");
+
 $(document).ready(function(){
+  $('.diagram').load('sources/skills.html #code');
 
-  var posts = new Array( ".pageStart", "#projects", "#me", "#skills", "#downloads", "#contact" );
-  var navPoints = new Array( "#pnp0", "#pnp1", "#pnp2", "#pnp3", "#pnp4", "#pnp5");
-  var scrollPos = 0;
-  var isMoving = false;
+  $('.projectItem').on('click', function(){
+    var pID = $(this).attr('id');
+    var num = pID.split('-');
 
-  $(posts[0]).velocity("scroll", 1000);
-  $(navPoints[0]).addClass("active");
-  $(navPoints[1]).removeClass("active");
-  $(navPoints[2]).removeClass("active");
-  $(navPoints[3]).removeClass("active");
-  $(navPoints[4]).removeClass("active");
-  $(navPoints[5]).removeClass("active");
-
-  function scrollToHandler(direction){
-    if(direction === "down"){
-      $('.top-body').addClass("closed-top-body");
-      $('.bottom-body').addClass("closed-bottom-body");
-
-      if(scrollPos < 5){
-        scrollPos += 1;
-        $(posts[scrollPos]).velocity("scroll", 1000);
-        $(navPoints[scrollPos-1]).toggleClass("active");
-        $(navPoints[scrollPos]).toggleClass("active");
-      }
-    }
-    else{
-      $('.top-body').removeClass("closed-top-body");
-      $('.bottom-body').removeClass("closed-bottom-body");
-
-      if(scrollPos > 0){
-        scrollPos -= 1;
-        $(posts[scrollPos]).velocity("scroll", 1000);
-        $(navPoints[scrollPos+1]).toggleClass("active");
-        $(navPoints[scrollPos]).toggleClass("active");
-      }
-    }
-    setTimeout(setMoving, 1000);
-  }
-
-  function setMoving(){
-    isMoving = false;
-  }
-
-  $('.page').bind('mousewheel', function(e){
-    e = window.event || e;
-    var delta = Math.max(-1, Math.min(1,
-      (e.wheelDelta || -e.deltaY || -e.detail)));
-
-    if(!isMoving){
-      if (delta < 0) {
-        isMoving = true;
-        scrollToHandler("down");
-      }else {
-        isMoving = true;
-        scrollToHandler("up");
-      }
-    }
+    $('.projectText').load('sources/projects.html #' + project[num[1]-1]);
+    $('.projectViewer').toggleClass('expand');
+    $('.projectInfo').toggleClass('showInfo');
+    $('.projectText').velocity("scroll", { duration: 400, offset: -200 });
   });
 
-  $('.page').bind('DOMMouseScroll', function(e){
-      if(!isMoving){
-        if (e.originalEvent.detail/120 > 0) {
-          isMoving = true;
-          scrollToHandler("down");
-        }else {
-          isMoving = true;
-          scrollToHandler("up");
-        }
-      }
-    });
+  $('.switchItem').on('click', function(){
+    var sID = $(this).attr('id');
+    var num = sID.split('-');
+
+    $('.diagram').load('sources/skills.html #' + skill[num[1]-1]);
+    $('.switchItem').find('selected').removeClass('selected');
+    $(this).addClass('selected');
+  });
+
+  $('.closer').on('click', function(){
+    $('.projectViewer').removeClass('expand');
+    $('.projectInfo').removeClass('showInfo');
+  });
 
   $('.mainNavItem').on('click', function(){
     var link = $(this);
     var val = link.attr('data-value');
     var data = val.split(':');
 
-    scrollPos = data[1] * 1;
     $(data[0]).velocity("scroll", 1000);
   });
+
 });
 
 $(window).scroll(function(){
@@ -88,5 +45,11 @@ $(window).scroll(function(){
   }  else{
     $('.pageNavigation').removeClass("naviStatic");
     $('.pageContent').removeClass("pageSubNavi");
+  }
+
+  if($(window).scrollTop() > ($(window).height() * 1.2)){
+    $('.posShift').addClass("shiftIn");
+  }  else{
+    $('.posShift').removeClass("shiftIn");
   }
 });
---
+++ index.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>
+      Portfolio
+    </title>
+    <meta charset="UTF-8" />
+    <link href='http://fonts.googleapis.com/css?family=Roboto:400,100' rel='stylesheet' type='text/css'>
+    <link href="assets/css/feather-webfont/feather.css" rel="stylesheet" />
+    <link href="assets/css/reset.css" rel="stylesheet" />
+    <link href="assets/css/forms.css" rel="stylesheet" />
+    <script src="assets/js/jquery.js" type="text/javascript"></script>
+    <script src="assets/js/velocity.js" type="text/javascript"></script>
+    <script src="assets/js/main.js" type="text/javascript"></script>
+  </head>
+  <body>
+    <div class="page">
+
+      <div class="pageStart">
+        <div id="laptop">
+          <div class="top-body Xcenter zdepth">
+            <div class="screen XYcenter">
+              <br>
+              lorenzkromer.<img src="./images/oe.png" alt="">
+              <br>
+              <br>
+              <button class="button">START</button>
+            </div>
+          </div>
+          <div class="bottom-body Xcenter zdepth">
+            <div class="Xcenter" id="openslot">&nbsp;</div>
+          </div>
+        </div>
+      </div>
+
+      <div class="pageNavigation zdepth">
+        <div class="logo Vcenter"><a href="http://lorenzkromer.at">http://lorenzkromer.at</a></div>
+        <div class="mainNavigation Vcenter">
+          <ul>
+            <li class="mainNavItem" data-value="#projects:1">PROJECTS</li>
+            <li class="mainNavItem" data-value="#me:2">ME</li>
+            <li class="mainNavItem" data-value="#skills:3">SKILLS</li>
+            <li class="mainNavItem" data-value="#downloads:4">DOWNLOADS</li>
+            <li class="mainNavItem" data-value="#contact:5">CONTACT</li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="pageContent">
+        <div class="post" id="projects">
+            <h3 class="Xcenter">PROJECTS</h3>
+            <p class="Xcenter">hier ein kleiner überblick über ein paar projekte, bei denen ich mitgearbeitet habe. falls euch gefällt was ihr seht, lasst es mich wissen ;-)</p>
+            <div class="projectViewer Xcenter">
+              <ul>
+                <li>
+                  <div class="projectItem" id="pi-1">
+                    <img src="./images/preis.jpg" alt="">
+                  </div>
+                </li>
+                <li>
+                  <div class="projectItem" id="pi-2">
+                    <img src="./images/ct.jpg" alt="">
+                  </div>
+                </li>
+                <li>
+                  <div class="projectItem" id="pi-3">
+                    <img src="./images/mapple.jpg" alt="">
+                  </div>
+                </li>
+                <li>
+                  <div class="projectItem" id="pi-4">
+                    <img src="./images/python.png" alt="">
+                  </div>
+                </li>
+                <li>
+                  <div class="projectItem" id="pi-5">
+                    <img src="./images/rpi.jpg" alt="">
+                  </div>
+                </li>
+                <li>
+                  <div class="projectItem" id="pi-6">
+                    <img src="./images/wiese.jpg" alt="">
+                  </div>
+                </li>
+              </ul>
+              <div class="projectInfo Xcenter">
+                <div class="projectText"></div>
+                <span class="icon-cross closer"></span>
+              </div>
+            </div>
+        </div>
+        <div class="post Xcenter" id="me">
+            <h3>THAT'S ME</h3>
+            <div class="posShift">
+              <div class="postLeft">
+                <p>hi! mein name ist lorenz kromer. ich bin 21 jahre alt, lebe in wien und studiere in st. pölten.</p>
+                <p>derzeit versuche ich endlich ein </p>
+              </div>
+              <div class="postRight">
+                <img src="./images/porttrait.jpg" alt="">
+              </div>
+              <div class="clearfix">&nbsp;</div>
+          </div>
+        </div>
+        <div class="post Xcenter" id="skills">
+            <h3>SKILLS</h3>
+            <div class="diagram Xcenter">
+              &nbsp;
+            </div>
+            <div class="switch Xcenter">
+              <div class="switchItem selected" id="sID-1">Code</div>
+              <div class="switchItem" id="sID-2">Design</div>
+              <div class="switchItem" id="sID-3">Usability</div>
+            </div>
+        </div>
+        <div class="post Xcenter" id="downloads">
+            <h3>DOWNLOADS</h3>
+            <p class="Xcenter">??? coming soon ???</p>
+        </div>
+        <div class="post Xcenter" id="contact">
+            <h3>CONTACT</h3>
+            <form action="#">
+              <input type="text" class="contactForm" placeholder="Name"><br>
+              <input type="text" class="contactForm" placeholder="Email"><br>
+              <textarea name="" class="contactForm ta" placeholder="Message" id="" cols="30" rows="10"></textarea><br>
+              <input type="submit" class="contactForm button" value="SEND MESSAGE"></input>
+            </form>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
